79a1c5a98dee1b7cb6c6499c0eb3e9c57e289fec
sandeep <sandeep2211ds@gmail.com>
2026-09-15 00:15:28 +0530

    Use environment database URL

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://sandeepsharma@localhost/bugzyme"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
