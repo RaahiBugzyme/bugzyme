ae5f47ccac8f0e1e9a38fedd26c90edb99da2f28
sandeep <sandeep2211ds@gmail.com>
2026-09-15 00:49:16 +0530

    Add CORS middleware for frontend

--- app/main.py
+++ app/main.py
@@ -25,9 +25,16 @@ from .connection_manager import ConnectionManager
 from .database import engine, Base, get_db
 from . import models, schemas
 from datetime import datetime
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 manager = ConnectionManager()
 
 
